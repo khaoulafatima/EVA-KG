25797f6c6a2d23318d26c2cdb870a7b856473043
PeppeRubini <giusepperubini5@gmail.com>
2023-11-19 10:29:59 +0100

    aggiunto all'endpoint locale controllo della query

--- src/endpoint.py
+++ src/endpoint.py
@@ -1,5 +1,6 @@
 from flask import Flask, request
 from rdflib import Graph
+from rdflib.plugins.sparql import prepareQuery
 from threading import Thread
 import os
 import time
@@ -28,29 +29,39 @@ def format_query_results(results):
     return results.serialize(format="json").decode("utf-8")
 
 
+def check_sparql_syntax(query_string):
+    try:
+        g = Graph()
+        query = prepareQuery(query_string)
+        g.query(query)
+        return True
+    except Exception as e:
+        print(f"Errore di sintassi SPARQL: {e}")
+        return False
+
+
 def endpoint_request():
     endpoint_url = 'http://localhost:5000/sparql'
-    time.sleep(5)
+    time.sleep(2)
     while True:
         sparql_query_string = input("\033[92mInserisci una query oppure \"exit\" per chiudere: \033[0m")
         if sparql_query_string == "exit":
-            os.system("taskkill /F /IM python.exe")
+            os.system("taskkill /F /IM python.exe > nul 2>&1")
         else:
             # sparql_query_string = "SELECT ?s ?p ?o WHERE { ?s ?p ?o } LIMIT 10"
             # sparql_query_string = "SELECT ?s ?p ?o WHERE { BIND(<http://eva.org/entity/40020/03> AS ?s) ?s ?p ?o }"
-            print(f"Query posta: {sparql_query_string}")
-            response = requests.get(endpoint_url, params={"query": sparql_query_string, "format": "json"})
-
-            if response.status_code == 200:
-                results = response.json()
-                pprint.pprint(results['results']['bindings'], sort_dicts=False)
-                print(f"\nOttenuti {len(results['results']['bindings'])} risultati\n")
-                time.sleep(2)
-            else:
-                print(f"Errore nella richiesta SPARQL: {response.status_code} - {response.text}")
+            if check_sparql_syntax(sparql_query_string):
+                response = requests.get(endpoint_url, params={"query": sparql_query_string, "format": "json"})
+                if response.status_code == 200:
+                    results = response.json()
+                    pprint.pprint(results['results']['bindings'], sort_dicts=False)
+                    print(f"\nOttenuti {len(results['results']['bindings'])} risultati\n")
+                    time.sleep(1)
+                else:
+                    print(f"Errore nella richiesta SPARQL: {response.status_code} - {response.text}")
 
 
 if __name__ == "__main__":
     er = Thread(target=endpoint_request)
     er.start()
-    app.run(host="localhost", port=5000, debug=True)
+    app.run(host="localhost", port=5000, debug=True, use_reloader=False)
